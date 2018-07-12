1a5638cf32e98382b0c7e7cdc3f2da901b48d0f3
irenealmazan <irenealmazan@hotmail.com>
2018-07-12 16:18:23 -0500

    correcting the same mistake than previous commit

--- NW_calc_dp_BCDI.m
+++ NW_calc_dp_BCDI.m
@@ -37,7 +37,7 @@ for ii = 1:numel(delta_thscanvals)
     
      mxI(ii) = max(simI(ii).I(:));
      
-     im_sum_sim = im_sum_sim + simI(ii).Psi_mod;    
+     im_sum_sim = im_sum_sim + simI(ii).I;    
     
      % store angles, dqshifts and diffraction patterns in structure
     data_exp(ii).dth_real = delta_thscanvals(ii)+dth_disp(ii);
@@ -47,7 +47,7 @@ for ii = 1:numel(delta_thscanvals)
     data_exp(ii).dshift_nominal = dq_shift_nominal;
     data_exp(ii).dqshift_real = dq_shift_real(ii,:);
     data_exp(ii).dqshift = dq_shift_real(ii,:); % initial value of dq
-    data_exp(ii).simI = simI(ii).Psi_mod;
+    data_exp(ii).simI = simI(ii).I;
     data_exp(ii).rock = rock_curve(ii);
     
     % plot:
@@ -62,7 +62,7 @@ for ii = 1:numel(delta_thscanvals)
             axis image;
             
             subplot(122);
-            imagesc(simI(ii).Psi_mod);
+            imagesc(simI(ii).I);
             axis image;
             colorbar;
             title([' ii = ' num2str(ii)]);
@@ -86,7 +86,7 @@ if plotResults
     axis image;
     
     subplot(221);
-    imagesc(simI(middpind).Psi_mod);
+    imagesc(simI(middpind).I);
     axis image;
     title([' ii = ' num2str(middpind)]);
     
